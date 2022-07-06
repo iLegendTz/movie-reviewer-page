bbece3576f3e8aa2c7445721decb86b372ca5144
iLegendTz <miguel.isaac@live.com>
2022-07-06 13:15:54 -0500

    [UPDATE] HandleLogin

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import { apiURL } from '../../api/API';
+
+import { useForm } from '../../hooks/useForm';
 
 import styles from './styles.module.css';
 
 export const Login = () => {
+  const {
+    form: { email, password },
+    handleFormChange,
+  } = useForm({ email: '', password: '' });
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const response = await axios
+      .post(`${apiURL}/users/login`, {
+        email: email,
+        password: password,
+      })
+      .then((value) => value)
+      .catch((error) => {
+        const { response } = error;
+
+        return response;
+      });
+  };
+
   return (
     <div className={styles.container}>
       <form className={styles.login_form}>
@@ -17,6 +44,8 @@ export const Login = () => {
             name="email"
             type={'email'}
             placeholder="example@correo.com"
+            value={email}
+            onChange={handleFormChange}
           />
         </div>
 
@@ -30,17 +59,23 @@ export const Login = () => {
             name="password"
             type={'password'}
             placeholder="password"
+            value={password}
+            onChange={handleFormChange}
           />
         </div>
 
         <div className="w-100 mb-3">
-          <button className="btn btn-primary w-100" type={'submit'}>
+          <button
+            className="btn btn-primary w-100"
+            type={'submit'}
+            onClick={handleLogin}
+          >
             Login
           </button>
         </div>
 
         <p className="text-center">
-          No tienes una cuenta? <a href="#">Registrate!</a>
+          No tienes una cuenta? <Link to={'/register'}>Registrate!</Link>
         </p>
       </form>
     </div>
