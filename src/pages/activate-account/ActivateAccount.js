6d35555ec6f8ab1d50ca4c9093f48e7fa07848d3
iLegendTz <miguel.isaac@live.com>
2022-07-09 12:00:25 -0500

    [ADD] ActivateAccount.js [ADD] ActivateAccount styles

---
+++ src/pages/activate-account/ActivateAccount.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+
+import { ApiResponseMessage } from '../../components/ApiResponseMessage';
+import { textToComponent } from '../../utils/textToComponent';
+
+import { apiURL } from '../../api/API';
+
+import styles from './styles.module.css';
+
+export const ActivateAccount = () => {
+  const [searchParams] = useSearchParams();
+  const { token } = Object.fromEntries([...searchParams]);
+
+  const [apiResponse, setApiResponse] = useState({
+    data: { message: '', code: '' },
+    error: null,
+    children: <></>,
+  });
+
+  useEffect(() => {
+    const handleActivated = async () => {
+      const response = await axios
+        .get(`${apiURL}/users/activate-user`, {
+          params: { token: token },
+        })
+        .then((value) => value)
+        .catch(({ response }) => response);
+
+      if (response.status !== 200) {
+        setApiResponse({
+          response: response.data,
+          error: true,
+          children: textToComponent(response.data.message),
+        });
+
+        return;
+      }
+
+      setApiResponse({
+        response: response.data,
+        error: false,
+        children: (
+          <>
+            {textToComponent(response.data.message)}{' '}
+            <Link to={'/login'}>Regresar</Link>
+          </>
+        ),
+      });
+    };
+    handleActivated();
+  }, [token]);
+
+  return (
+    <div className={styles.container}>
+      <ApiResponseMessage apiResponse={apiResponse} />
+    </div>
+  );
+};
