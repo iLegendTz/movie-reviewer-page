d8fa89685e04c393f3daf56b6849db1e7aaf32e7
iLegendTz <miguel.isaac@live.com>
2022-07-09 12:01:59 -0500

    [UPDATE] Agregando route /activate-account

--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { ActivateAccount } from '../pages/activate-account/ActivateAccount';
 
 import { Login } from '../pages/login/Login';
 import { Register } from '../pages/register/Register';
@@ -9,6 +10,7 @@ export const PublicRoutes = () => {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/activate-account" element={<ActivateAccount />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
