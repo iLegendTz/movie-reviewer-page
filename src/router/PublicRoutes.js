b90d497727fe2cf16c71d0e351e0057336fed77a
iLegendTz <miguel.isaac@live.com>
2022-07-10 14:20:44 -0500

    [ADD] Home.js [UPDATE] index Route

--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ActivateAccount } from '../pages/activate-account/ActivateAccount';
+import { Home } from '../pages/home/Home';
 
 import { Login } from '../pages/login/Login';
 import { Register } from '../pages/register/Register';
@@ -8,10 +9,12 @@ import { Register } from '../pages/register/Register';
 export const PublicRoutes = () => {
   return (
     <Routes>
+      <Route index element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/activate-account" element={<ActivateAccount />} />
-      <Route path="*" element={<Navigate to="/login" replace />} />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
