68e226f7523894b3e0d7f935bfc98d118cb6ae62
iLegendTz <miguel.isaac@live.com>
2022-07-11 21:39:39 -0500

    [ADD] MovieDBAPI.js

---
+++ src/api/MovieDBAPI.js
@@ -0,0 +1,3 @@
+export const apiURLMovieDB = 'https://api.themoviedb.org/3';
+export const apiKeyMovieDB = '90c4072548ca0164fe4f78f8dad1b6c4';
+export const imagesURL = 'https://image.tmdb.org/t/p';
