7feed82e426aa5b8d682a865dfcca2c2fad6f47c
iLegendTz <miguel.isaac@live.com>
2022-07-11 21:39:05 -0500

    [ADD] Poster.js

---
+++ src/components/Poster.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Sizes "w92","w154","w185","w342","w500","w780","original"
+// Los sizes se enlistan en https://api.themoviedb.org/3/configuration en la variable "poster_sizes"
+export const Poster = ({ size = 'original', filePath, styles }) => {
+  // console.log(style);
+  return (
+    <img
+      className={`${styles}`}
+      src={`https://image.tmdb.org/t/p/${size}/${filePath}`}
+      alt={'poster'}
+    />
+  );
+};
