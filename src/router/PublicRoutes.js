03c3730a79c2f4d36f4c031467c7dc8170f3e89d
iLegendTz <miguel.isaac@live.com>
2022-07-16 19:48:51 -0500

    [UPDATE] Agregando route /search

--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import { ActivateAccount } from '../pages/activate-account/ActivateAccount';
 import { Home } from '../pages/home/Home';
-
 import { Login } from '../pages/login/Login';
 import { Register } from '../pages/register/Register';
+import { Search } from '../pages/search/Search';
 
 export const PublicRoutes = () => {
   return (
@@ -13,6 +14,7 @@ export const PublicRoutes = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/activate-account" element={<ActivateAccount />} />
+      <Route path="/search" element={<Search />} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
