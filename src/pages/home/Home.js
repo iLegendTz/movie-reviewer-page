a8d4c2a2634412e60b8f428d195e2a4dca3fe87d
iLegendTz <miguel.isaac@live.com>
2022-07-16 19:49:13 -0500

    [UPDATE] Mostrando carousels y searchbar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+import { MoviesCarousel } from '../../components/MoviesCarousel';
+import { SearchBar } from '../../components/SearchBar';
+import { TvShowsCarousel } from '../../components/TvShowsCarousel';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <SearchBar />
+
+      <div className="mb-2">
+        <h2>Peliculas</h2>
+        <MoviesCarousel />
+      </div>
+
+      <div className="mb-2">
+        <h2>Programas de TV</h2>
+        <TvShowsCarousel />
+      </div>
+    </div>
+  );
 };
