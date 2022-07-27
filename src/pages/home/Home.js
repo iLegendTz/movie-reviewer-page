9d6fc8c8ac4b83937e6fca365ce96570e092fc67
iLegendTz <miguel.isaac@live.com>
2022-07-27 12:01:48 -0500

    [UPDATE] Creando fn_search. [UPDATE] Se pasa fn_search al componente SearchBar.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 import { MoviesCarousel } from '../../components/MoviesCarousel';
 import { SearchBar } from '../../components/SearchBar';
 import { TvShowsCarousel } from '../../components/TvShowsCarousel';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const fn_search = (query) => {
+    navigate({
+      pathname: '/search',
+      search: createSearchParams({ query: query }).toString(),
+    });
+  };
+
   return (
     <div className="container">
-      <SearchBar />
+      <SearchBar fn_search={fn_search} />
 
       <div className="mb-2">
         <h2>Peliculas</h2>
