e588d8abeea4bb74c07df1ec51c505254f6a1bc3
iLegendTz <miguel.isaac@live.com>
2022-07-27 12:40:34 -0500

    [FIX] En caso de no tener poster img se pone una imagen default

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import black_background from '../assets/black_background.png';
+
 // Sizes "w92","w154","w185","w342","w500","w780","original"
 // Los sizes se enlistan en https://api.themoviedb.org/3/configuration en la variable "poster_sizes"
 export const Poster = ({ size = 'original', filePath, styles }) => {
+  if (!filePath) {
+    return (
+      <img className={`${styles}`} src={`${black_background}`} alt={'poster'} />
+    );
+  }
+
   return (
     <img
       className={`${styles}`}
